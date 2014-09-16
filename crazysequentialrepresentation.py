619107b7cfa4206817e5c162ff44888d98bf813d
T.J. Eckman <jeek@jeek.net>
2014-09-16 17:34:49 -0400

    Added factorial and sqrt

--- crazysequentialrepresentation.py
+++ crazysequentialrepresentation.py
@@ -4,6 +4,51 @@ from math import log10
 
 upperlimit = 1000000
 
+def is_square(i):
+    if i < 2:
+        return False
+    sqrtp = 1
+    upper = i
+    lower = 1
+    while upper > lower + 1:
+        sqrtp = (upper + lower) / 2
+#        print sqrtp
+        if sqrtp * sqrtp < i:
+            lower = sqrtp
+        else:
+            upper = sqrtp
+    if sqrtp * sqrtp == i:
+        return True
+    return False
+
+def sqrt(i):
+    if i < 2:
+        return False
+    sqrtp = 1
+    upper = i
+    lower = 1
+    while upper > lower + 1:
+        sqrtp = (upper + lower) / 2
+#        print sqrtp
+        if sqrtp * sqrtp < i:
+            lower = sqrtp
+        else:
+            upper = sqrtp
+    if sqrtp * sqrtp == i:
+        return sqrtp
+
+
+def fac(i):
+    if i == 0:
+        return 1
+    if i == 2:
+        return 2
+    answer = 1
+    while i:
+        answer *= i
+        i -= 1
+    return answer
+
 for (outputfilename, queue) in [("csrup.txt", [dumps(range(1,10))]), ("csrdown.txt", [dumps(range(9,0,-1))])]:
 
     path = {}
@@ -46,6 +91,34 @@ for (outputfilename, queue) in [("csrup.txt", [dumps(range(1,10))]), ("csrdown.t
                 path[dumps(temp)] = path[dumps(current)]
                 temp2 = loads(path[dumps(temp)])
                 temp2.insert(j, "(" + str(temp2.pop(j)) + " ** " + str(temp2.pop(j)) + ")")
+                path[dumps(temp)] = dumps(temp2)
+
+
+        for j in range(len(current)):
+            temp = copy(current)
+            if temp[j] > 1 and is_square(temp[j]):
+                temp.insert(j, sqrt(temp.pop(j)))
+                path[dumps(temp)] = path[dumps(current)]
+                if dumps(temp) not in alreadydone:
+                    queue.append(dumps(temp))
+
+                path[dumps(temp)] = path[dumps(current)]
+                temp2 = loads(path[dumps(temp)])
+                temp2.insert(j, "sqrt(" + str(temp2.pop(j)) + ")")
+                path[dumps(temp)] = dumps(temp2)
+
+
+        for j in range(len(current)):
+            temp = copy(current)
+            if temp[i] == 0 or (temp[j] > 2 and temp[j] < 30):
+                temp.insert(j, fac(temp.pop(j)))
+                path[dumps(temp)] = path[dumps(current)]
+                if dumps(temp) not in alreadydone:
+                    queue.append(dumps(temp))
+
+                path[dumps(temp)] = path[dumps(current)]
+                temp2 = loads(path[dumps(temp)])
+                temp2.insert(j, "fac(" + str(temp2.pop(j)) + ")")
                 path[dumps(temp)] = dumps(temp2)
 
         for j in range(len(current) - 1):
