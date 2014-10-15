1c85ae8d23287229eba5a98e55ccdcac58b5c2bc
T.J. Eckman <jeek@jeek.net>
2014-10-15 05:26:58 -0400

    Fixed error in concatentation

--- mult/csr.py
+++ mult/csr.py
@@ -3,7 +3,7 @@ from copy import deepcopy as copy
 from multiprocessing import Pool
 import shelve
 upperlimit = 1000000000
-number_of_processes = 8
+number_of_processes = 4
 
 def is_square(i):
     if i < 2:
@@ -39,23 +39,23 @@ def recur(numbers, strings):
         # Unary Minus
         temp1 = copy(numbers)
         temp2 = copy(strings)
-        temp1[i] *= -1
-        temp2[i] = "-(" + temp2[i] + ")"
+        temp1.insert(i, temp1.pop(i) * -1)
+        temp2.insert(i, "-(" + temp2.pop(i) + ")")
         answers.append((temp1, temp2))
         # Factorial
         temp1 = copy(numbers)
         temp2 = copy(strings)
         if temp1[i] == 0 or (temp1[i] > 2 and temp1[i] < 30):
-            temp1[i] = fac(temp1[i])
-            temp2[i] = "(" + temp2[i] + ")!"
-        answers.append((temp1, temp2))
+            temp1.insert(i, fac(temp1.pop(i)))
+            temp2.insert(i, "(" + temp2.pop(i) + ")!")
+            answers.append((temp1, temp2))
         # Square Root
         temp1 = copy(numbers)
         temp2 = copy(strings)
         if is_square(temp1[i]):
-            temp1[i] = int(temp1[i] ** .5)
-            temp2[i] = "sqrt(" + temp2[i] + ")"
-        answers.append((temp1, temp2))
+            temp1.insert(i, int(temp1.pop(i) ** .5))
+            temp2.insert(i, "sqrt(" + temp2.pop(i) + ")")
+            answers.append((temp1, temp2))
     for i in range(len(numbers) - 1):
         # Addition
         temp1 = copy(numbers)
@@ -106,7 +106,7 @@ def recur(numbers, strings):
 if __name__ == "__main__":
     pool = Pool(processes = number_of_processes)
     iii = 10
-    while iii < 1000000000:
+    while iii < 10 ** 10:
 #    while iii < 40:
         seen = shelve.open("seen" + str(iii) + ".txt")
         queue = shelve.open("queue" + str(iii) + ".txt")
@@ -115,29 +115,32 @@ if __name__ == "__main__":
         while jj < len(queues):
             for kk in range(len(queues[jj][0]) - 1):
                 temp = copy(queues[jj])
-#                print temp
-                temp[0].insert(kk, temp[0].pop(kk) + temp[0].pop(kk))
-                temp[1].insert(kk, (str(temp[1].pop(kk) + str(temp[1].pop(kk)))))
-                if temp not in queues:
-                    queues.append(temp)                
+                if temp[0][kk] != 0:
+                    temp[0].insert(kk, int(str(temp[0].pop(kk)) + str(temp[0].pop(kk))))
+                    temp[1].insert(kk, (str(temp[1].pop(kk)) + str(temp[1].pop(kk))))
+                    if temp not in queues:
+                        queues.append(temp)                
             jj += 1
         for (ii, jj) in queues:
 #            print ii, jj
-            queue[str(ii)] = jj
+            if len(ii) > 1:
+                queue[str(ii)] = jj
 #        queue[str([1,2,3,4,5,6,7,8,9])] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         done = False
         while not done:
 #            print iii, len(seen), "/", len(queue), "\r",
             done = True
             results = []
-            for h in [i for i in queue if i not in seen][:number_of_processes ** 2]:
+            for h in [i for i in queue if i not in seen][:1024]:
+#            for h in [i for i in queue if i not in seen][:number_of_processes ** 2]:
 #                if h not in seen:
                     seen[h] = True
                     results.append(pool.apply_async(recur, (h, queue[h])))
 #                del queue[h]
     #        print len(results)
-            while len(results):
+            while len(results) > 0:
                 for k in results.pop().get():
+#                    print k, [i for i in seen]
                     if str(k[0]) not in queue:
                         queue[str(k[0])] = k[1]
 #                        if len(eval(k[0])) == 1:
